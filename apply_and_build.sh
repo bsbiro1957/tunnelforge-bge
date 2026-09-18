a139307d1e0ab1cf50c89e13724e1e613637bcaf
bsbiro1957 <bsbiro@gmail.com>
2026-09-18 16:30:16 +0200

    Update apply_and_build.sh

--- apply_and_build.sh
+++ apply_and_build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+# Patch alkalmazása
 git apply bge_dh2.patch
+
+# Függőségek letöltése és build
 flutter pub get
 cd android/gvisor
 go mod download
 cd ../..
 make build-debug
+
 echo "APK: build/app/outputs/flutter-apk/app-debug.apk"
